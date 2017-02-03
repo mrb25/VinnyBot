4d5ce12163ed8289539fc4fa8f81d5583d1acc26
JessWalters <jessw@iastate.edu>
2017-02-03 00:27:31 -0600

    Most functions up and running

--- Core/help.py
+++ Core/help.py
@@ -3,9 +3,9 @@
 async def help(client, message):
     user = message.author
     text = "`MEME COMMANDS\n"
-    text += "~meme\n~meme:textline1:textline2\n~kitten\n~eyebleach\n~shit\n~harambe\n~kappa\n~ayy\n~giphy search\n~rr subreddit -- Gives random HOT post from given subreddit\n"
+    text += "\n~shit\n~harambe\n~kappa\n~ayy\n~giphy search\n~rr subreddit -- Gives random HOT post from given subreddit\n"
     text += "~tr subreddit -- Top HOT post from subreddit right now\n"
-    text += "\nMODERATION COMMANDS\n~prune num\n~prune num @usernames~kick @username\n~ban @username\n~botname newusername\n~botavatar pngfilepath\n~whois @username -- Gives info about a user\n"
+    text += "\nMODERATION COMMANDS\n~prune num\n~kick @username\n~whois @username -- Gives info about a user\n"
     text += "\nVOICE COMMANDS\n~summon\n~play youtubeurl\n`"
     text += ""
     await client.send_message(user, text)
