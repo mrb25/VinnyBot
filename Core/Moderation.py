3e60869a23509b8c6f34f362fcaf0110d29db950
JessWalters <jessw@iastate.edu>
2017-02-02 21:04:03 -0600

    Adding more parser functions but not runnable yet

--- Core/Moderation.py
+++ Core/Moderation.py
@@ -13,3 +13,46 @@ async def kick(message, client):
         for i in range(0, len(message.mentions)):
             await client.kick(message.mentions[i])
     await client.send_message(message.channel, text)
+
+async def ban(message, client):
+    if len(message.mentions) == 0:
+        text = "You must @mention a user to kick."
+        await client.send_message(message.channel, text)
+    elif len(message.mentions) > 1:
+        text = "You can only ban a single user at a time."
+        await client.send_message(message.channel, text)
+    else:
+        await client.ban(message.mentions[0], 0)
+        await client.send_message(message.channel, message.mentions[0].name + 'has been successfully banned')
+
+async def count(message, client):
+    counter = 0
+    tmp = await client.send_message(message.channel, 'Calculating messages...')
+    async for log in client.logs_from(message.channel, limit=1000):
+        if log.author == message.author:
+            counter += 1
+
+    await client.edit_message(tmp, message.author.name + ', you have {} messages.'.format(counter))
+
+async def userInfo(message, client):
+    text = ''
+    if len(message.mentions) == 0:
+        text = "You must @mention a user."
+    elif len(message.mentions) > 1:
+        text = "You may only @mention one user."
+    else:
+        user = message.mentions[0]
+        text += user.mention + "\n"
+        text += "avatar url: " + user.avatar_url + "\n"
+        text += "username: " + user.name + "\n"
+        text += "joined: " + str(user.joined_at) + "\n"
+    await client.send_message(message.channel, text)
+
+async def avatar(message, client):
+    if len(message.mentions) == 0:
+        text = "You must @mention a user."
+    elif len(message.mentions) > 1:
+        text = "You may only @mention one user."
+    else:
+        text = message.mentions[0].mention + " " + message.mentions[0].avatar_url
+    await client.send_message(message.channel, text)
