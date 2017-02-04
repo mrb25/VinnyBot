2ce90b3de1576d6df1d18c8380eaba5d1bc1853b
JessWalters <jessw@iastate.edu>
2017-02-04 17:39:17 -0600

    Lambda expressions added to call finish playing method

--- Core/VoiceCore.py
+++ Core/VoiceCore.py
@@ -40,7 +40,7 @@ async def playTest(message, client):
                 player = playerMap[vClient]
                 vidUrl = message.content
                 vidUrl = re.search("(?P<url>https?://[^\s]+)", vidUrl).group("url")
-                player = await vClient.create_ytdl_player(vidUrl, use_avconv=True, after=songFinished(message,client))
+                player = await vClient.create_ytdl_player(vidUrl, use_avconv=True, after=lambda: songFinished(message,client))
                 """Adding player to hashmap"""
                 playerMap[vClient] = player
                 player.start()
@@ -51,7 +51,7 @@ async def playTest(message, client):
             vClient = client.voice_client_in(message.server)
             vidUrl = message.content
             vidUrl = re.search("(?P<url>https?://[^\s]+)", vidUrl).group("url")
-            player = await vClient.create_ytdl_player(vidUrl, use_avconv=True, after=songFinished(message,client))
+            player = await vClient.create_ytdl_player(vidUrl, use_avconv=True, after=lambda: songFinished(message,client))
             """Adding player to hashmap"""
             player.use_avconv = True
             playerMap[vClient] = player
