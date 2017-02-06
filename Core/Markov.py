400512f73affe0470753d599a31ebea42f2ba237
JessWalters <jessw@iastate.edu>
2017-02-06 01:59:43 -0600

    Added beta Markov chain support

--- Core/Markov.py
+++ Core/Markov.py
@@ -9,13 +9,11 @@ async def generateMarkovComment(message, client):
         return
 
     tmp = await client.send_message(message.channel, 'Downloading messages...')
+
     async for log in client.logs_from(message.channel, limit=5000):
         if log.author == message.mentions:
             counter += 1
             textSource += ' ' + message.content + ' '
-
-    if (counter < 30):
-        await client.send_message(message.channel, 'Too few messages received. Cannot generate a message from user')
 
     await client.edit_message(tmp, 'Generating comment from {} messages.'.format(counter))
 
