aabea1c8d6932cef2910c9be25dbdd044c1c7a64
JessWalters <jessw@iastate.edu>
2017-02-06 21:04:53 -0600

    Improvments to Markov Chain, added pause/resume to audio stream

--- Core/Markov.py
+++ Core/Markov.py
@@ -18,7 +18,7 @@ async def generateMarkovComment(message, client):
                 textSource += log.content + '\n'
                 # print('message received')
         channelCounter += 1
-        updateLoading(message, channel, tmp, channelCounter)
+        await updateLoading(message, channel, tmp, channelCounter)
 
 
     if counter <= 30:
