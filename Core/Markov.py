d975e0fbff25cc5ffcc0047e90c4c4dcc164c34a
JessWalters <jessw@iastate.edu>
2017-02-09 03:18:33 -0600

    Markov from channel support

--- Core/Markov.py
+++ Core/Markov.py
@@ -9,6 +9,9 @@ async def generateMarkovComment(message, client):
     textSource = ''
     usableChannels = []
 
+    if len(message.channel_mentions) > 0:
+        generateMarkovChannel(message, client)
+
     if (len(message.mentions) != 1):
         await client.send_message(message.channel, 'Please mention only 1 user')
         return
@@ -24,6 +27,7 @@ async def generateMarkovComment(message, client):
 
     print('Comment for ' + message.mentions[0].name + ' requested by ' + message.author.name + ' in Server: '
           + message.server.name + ' in channel: ' + message.channel.name)
+
     tmp = await client.send_message(message.channel, 'Downloading messages...')
 
     await updateLoading(message, client, tmp, channelCounter, usableChannels)
@@ -66,3 +70,51 @@ async def updateLoading(message, client, tmp, channelCounter, usableChannels):
 
     loading += '|'
     await client.edit_message(tmp, loading)
+
+async def updateChannelLoading(message, client, tmp, counter):
+    loading = 'Downloading messages from channel ({}/10,000) |'.format(counter)
+
+    for x in range(0, counter/1000):
+        loading += '█'
+    for x in range(0, (10999-counter)/1000):
+        loading += '▒'
+
+    loading += '|'
+    await client.edit_message(tmp, loading)
+
+async def generateMarkovChannel(message, client):
+
+    textSource = ''
+    counter = 0
+    usableCounter = 0
+
+    if len(message.channel_mentions) != 1:
+        await client.send_message(message.channel, 'Please mention only one channel')
+        return
+
+    tmp = await client.send_message(message.channel, 'Downloading messages from Channel... (0%)')
+
+    async for log in client.logs_from(message.channel_mentions[0], limit=10000):
+        # Checks and omits commands
+        if not log.content.startswith('~'):
+            usableCounter += 1
+            textSource += log.content + '\n'
+
+        counter += 1
+        if counter % 500 == 0:
+            await updateChannelLoading(message, client, tmp, counter)
+
+    if usableCounter <= 30:
+        await client.send_message(message.channel, 'Not enough messages received, cannot generate message')
+        return
+
+    await client.edit_message(tmp, 'Generating comment from {} messages.'.format(counter))
+    print('Generating model from {} messages'.format(counter))
+
+    text_model = markovify.NewlineText(textSource)
+    # Debugging print
+    # print(textSource)
+    generatedMessage = text_model.make_sentence(tries=100)
+    print('Generated message: ' + generatedMessage + '\n')
+
+    await client.edit_message(tmp, message.channel_mentions[0].name + ' says: ' + generatedMessage.replace('@', ':at:'))
