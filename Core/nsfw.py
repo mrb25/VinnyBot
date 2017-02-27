27d49aab30d8758c7b22f0b60a64510f87a42db8
JessWalters <jessw@iastate.edu>
2017-02-26 19:49:17 -0600

    Tag regulation fix

--- Core/nsfw.py
+++ Core/nsfw.py
@@ -2,15 +2,16 @@ import urllib.request
 import discord
 import re
 import asyncio
+import pybooru
 from random import randint
 import xml.etree.ElementTree
 
 async def postR34(message, client):
     tags = message.content[4:]
 
-    if "fur" not in tags or "furry" not in tags or "yiff" not in tags:
+    if "fur" not in tags or "furry" not in tags or "yiff" not in tags or "anthro" not in tags:
         search = "http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={}".format(tags) +\
-                 " -fur -furry -yiff"
+                 " -fur -furry -yiff -anthro"
     else:
         search = "http://rule34.xxx/index.php?page=dapi&s=post&q=index&limit=100&tags={}".format(tags)
 
@@ -18,13 +19,17 @@ async def postR34(message, client):
     e = xml.etree.ElementTree.parse(xmlFile)
     root = e.getroot()
 
-    picUrl = []
+    picUrls = []
 
     for post in root.findall('post'):
-        picUrl.append('http:' + post.get('file_url'))
+        if "furaffinity" not in post.get('source'):
+            picUrls.append('http:' + post.get('file_url'))
 
-    if len(picUrl) == 0:
+    for post in root.findall('post'):
+        picUrls.append('http:' + post.get('file_url'))
+
+    if len(picUrls) == 0:
         await client.send_message(message.channel, "No results, try searching something less retarded next time!")
 
     else:
-        await client.send_message(message.channel, picUrl[randint(0, len(picUrl)-1)])
+        await client.send_message(message.channel, picUrls[randint(0, len(picUrls) - 1)])
