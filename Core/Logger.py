390ab39bcbc5076e140f340078f4832eb0028c4f
JessWalters <jessw@iastate.edu>
2017-03-08 00:32:28 -0600

    Fixed raspian io bug

--- Core/Logger.py
+++ Core/Logger.py
@@ -5,7 +5,6 @@ def logCommand(message, client, command):
     if not os.path.exists("logs"):
         os.makedirs("logs")
 
-    f = open(time.strftime("logs\VinnyLog %m-%d-%Y.txt"), "a+")
-    f.write(time.strftime("[%H:%M:%S] ") + command + " called by: " + message.author.name + " in channel: " +
+    with open(time.strftime("logs\VinnyLog %m-%d-%Y.txt"), "a+") as f:
+        f.write(time.strftime("[%H:%M:%S] ") + command + " called by: " + message.author.name + " in channel: " +
             message.channel.name + " in server: " + message.server.name + "\n")
-    f.close()
