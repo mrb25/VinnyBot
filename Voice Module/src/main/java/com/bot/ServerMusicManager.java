404a1d982fd34983c8d973cd45a073d63f747fa5
JessWalters <jessw@iastate.edu>
2017-03-28 19:11:29 -0500

    Voice migrating to Java

---
+++ Voice Module/src/main/java/com/bot/ServerMusicManager.java
@@ -0,0 +1,33 @@
+package com.bot;
+
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayerManager;
+import com.sedmelluq.discord.lavaplayer.player.AudioPlayer;
+
+/**
+ * Created by Jess on 3/28/2017.
+ */
+public class ServerMusicManager {
+
+    public final AudioPlayer player;
+    /**
+     * Track scheduler for the player.
+     */
+    public final TrackScheduler scheduler;
+
+    /**
+     * Creates a player and a track scheduler.
+     * @param manager Audio player manager to use for creating the player.
+     */
+    public ServerMusicManager(AudioPlayerManager manager) {
+        player = manager.createPlayer();
+        scheduler = new TrackScheduler(player);
+        player.addListener(scheduler);
+    }
+
+    /**
+     * @return Wrapper around AudioPlayer to use it as an AudioSendHandler.
+     */
+    public AudioPlayerSendHandler getSendHandler() {
+        return new AudioPlayerSendHandler(player);
+    }
+}
