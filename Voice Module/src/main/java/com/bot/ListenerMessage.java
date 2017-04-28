201ce4c37c9a2c6d0dd511aa7869c5b186880012
Jess Walters <jessw@iastate.edu>
2017-04-27 22:29:34 -0500

    Incomplete voice update

---
+++ Voice Module/src/main/java/com/bot/ListenerMessage.java
@@ -0,0 +1,14 @@
+package com.bot;
+
+import javafx.scene.media.AudioTrack;
+import net.dv8tion.jda.core.entities.TextChannel;
+
+/**
+ * Created by sable_000 on 4/27/2017.
+ */
+public class ListenerMessage {
+
+    public ListenerMessage(long authorID, com.sedmelluq.discord.lavaplayer.track.AudioTrack[] tracks, TextChannel channel){
+
+    }
+}
