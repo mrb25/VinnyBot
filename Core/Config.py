532414141572520a283894670a1ab985bc676879
JessWalters <jessw@iastate.edu>
2017-05-23 00:11:42 -0500

    Rewrote help. Now all commands stored in file and help is now in config.py as if vinny wasnt confusing enough

--- Core/Config.py
+++ Core/Config.py
@@ -1,8 +1,9 @@
 import os
+import discord
+
 
 tokens = {}
 commands = {}
-info = {}
 VINNY_COLOR = int('008cba', 16)
 
 
@@ -22,17 +23,50 @@ def initConfig():
     with open("config/commands.txt", "r") as f:
         map = str.maketrans('', '', '*\n')
         for line in f:
-            if line.startswith('***'):
+            if line.startswith("***"):
                 name = line.split(' ')[0].lower().translate(map)
                 commands[name] = {}
-                while True:
-                    full = next(f)
-                    if full.startswith('***') or full.startswith("END"):
-                        break
-                    command = full.split(' ')[0].replace('~', '')
+                continue
+            if name is not None:
+                if line != "\n":
+                    command = line.split(' ')[0].replace('~', '')
                     print(command)
                     commands[name][command] = line
 
+async def help(message, client):
+    if len(message.content.split(" ")) > 1:
+        for i in range(1, len(message.content.split(" "))):
+            lookFor = message.content.split(" ")[i]
+            await client.send_message(message.author, lookFor + ": " + getCommand(lookFor))
+    else:
+        await client.send_message(message.author, embed=defaultHelp(client))
+
+
+def getCommand(command):
+    for key, value in commands.items():
+        try:
+            if value[command] is not None:
+                return value[command]
+        except KeyError:
+            continue
+
+    return "Command not found"
+
+
+def defaultHelp(client):
+    embed = discord.Embed(title='Command List', colour=VINNY_COLOR)
+    embed.add_field(name="Note", value="In all commands with *search_terms* replace it with what you want to search."
+                                       "For a tag that is multiple words use _ instead of spaces.", inline=False)
+    for key, value in commands.items():
+        list = ""
+        for command, desc in value.items():
+            list += desc
+        embed.add_field(name=key.upper() + " COMMANDS",
+                        value=list,
+                        inline=False)
+
+    embed.set_author(name=client.user.name, icon_url=client.user.avatar_url)
+    return embed
 
 def getToken(tokenName):
     return tokens[tokenName]
--- Core/help.py
+++ Core/help.py
@@ -1,58 +1,6 @@
 import discord
 
-memeCommands = {}
-moderationCommands = {}
-voiceCommands = {}
-nsfwCommands = {}
-redditCommands = {}
 VINNY_COLOR = int('008cba', 16)
-ABOUT_VINNY = ""
-VINNY_INVITE = ""
-VINNY_SERVER = ""
-
-
-async def help(client, message):
-    user = message.author
-    text = "```MEME COMMANDS\n"
-    text += "\n~shit ~harambe ~lenny ~hammer ~doggo ~hitler ~mario ~megaman ~salt ~feels ~pikachu" \
-            " ~ayy\n~giphy *search terms*\n~8ball -- Get a response from the magic 8 ball\nREDDIT COMMANDS" \
-            "\n~rr *subreddit* -- Gives random HOT post from given subreddit\n"
-    text += "~tr *subreddit* -- Top HOT post from subreddit right now\n~cosplay *search_terms* -- Searches for a " \
-            "cosplay meeting the search terms. (use _ instead of spaces)\n~cosplaygirls *search_terms* -- Searches for" \
-            "a girl cosplay using the search terms (use _ instead of spaces)\n" \
-            "\nCOMMENT COMMANDS\n~comment @user or #channel -- Generates a unique comment based on the user/channel " \
-            "post history (Experimental)\n" \
-            "~ryzen -- Hey did you hear about Ryzen??\n"
-    text += "\nMODERATION COMMANDS\n~prune @user \"example\" *num* -- Removes the messages from the last *num* messages " \
-            "from user containing \"example\". Mentioning users and defining text are both optional and can be used for as many " \
-            "users or phrases as needed in a single prune command" \
-            "\n~kick @username\n~whois @username -- Gives info about a user\n" \
-            "~stats -- Gives stats about Vinny\n~info -- Gives info about Vinny\n~invite -- Gives the link to invite Vinny to a server"
-    text += "\nVOICE COMMANDS\n"
-    text += "~play *URL* -- Plays audio from video in your channel. Works well with Youtube, Soundcloud, Twitch," \
-            " etc\n~search -- Searches youtube and gives a list of videos to choose from\n" \
-            "~cancel -- Cancels a search request\n~playlist -- Gets the playlist of currently playing music\n" \
-            "~skip -- Skips the current song\n~leave -- Makes Vinny leave your voice channel"
-    text += "\n~stop -- If an audio stream is playing in your server it stops it\n" \
-            "~pause -- Pauses current audio stream\n~resume -- Resumes audio stream when paused" \
-            "\n~voicestats -- Gives info about current audio streams" \
-            "\n\nNSFW COMMANDS\n~togglensfw -- Toggles the 'NSFW lock' on each channel" \
-            " (User must have 'Manage Channels' permission)\n~nsfw -- Tells whether or not" \
-            "nsfw is enabled on a given channel\n~r34 *tags* (for multi-word tags replace spaces with _) -- Rule 34\n\n" \
-            "Discord Server -- https://discord.gg/XMwyzxZ```\n"
-    await client.send_message(user, text)
-
-async def info(message, client):
-    info = discord.Embed(title='Info', colour=VINNY_COLOR)
-    info.set_author(name=client.user.name, icon_url=client.user.avatar_url)
-    info.add_field(name="About Vinny", value=ABOUT_VINNY)
-    info.add_field(name="Vinny Server", value=VINNY_SERVER)
-    info.add_field(name="Invite Vinny", value=VINNY_INVITE)
-    await client.send_message(message.channel, embed=info)
-
-async def invite(message, client):
-    await client.send_message(message.channel, VINNY_INVITE)
-
 ABOUT_VINNY = "Vinny is an under development discord bot created by Kikkia. Since Vinny is under current development" \
               "Vinny may rarely crash or drop offline for short periods of time as I find/fix bugs and add features. " \
               "Vinny offers a wide set of features for " \
@@ -65,6 +13,19 @@ VINNY_SERVER = "Vinny has his own discord server where you can suggest features,
                "other users and developers of vinny. Invite Link: https://discord.gg/XMwyzxZ"
 
 VINNY_INVITE = "To invite Vinny to your server go to: https://discordapp.com/oauth2/authorize?client_id=276855867796881408&scope=bot&permissions=67628096"
+
+async def info(message, client):
+    info = discord.Embed(title='Info', colour=VINNY_COLOR)
+    info.set_author(name=client.user.name, icon_url=client.user.avatar_url)
+    info.add_field(name="About Vinny", value=ABOUT_VINNY)
+    info.add_field(name="Vinny Server", value=VINNY_SERVER)
+    info.add_field(name="Invite Vinny", value=VINNY_INVITE)
+    await client.send_message(message.channel, embed=info)
+
+async def invite(message, client):
+    await client.send_message(message.channel, VINNY_INVITE)
+
+
 
 
 
